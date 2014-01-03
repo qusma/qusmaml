2964961ac2f87e13806ff069096d53b8bced1b76
qusma <qusmablog@gmail.com>
2014-01-03 17:14:36 +0200

    Added DBA class.

---
+++ QUSMAML/DBA.cs
@@ -0,0 +1,40 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="DBA.cs" company="">
+// Copyright 2014 Alexander Soffronow Pagonidis
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+//DTW Barycenter Averaging
+using NDtw;
+
+namespace QUSMAML
+{
+    public class DBA
+    {
+        private readonly Func<double[], double[], double> _distanceFunc;
+
+        public DBA(Func<double[], double[], double> distanceFunc)
+        {
+            _distanceFunc = distanceFunc;
+        }
+
+        public double[] Average(double[] average, List<double[]> series)
+        {
+
+            
+
+            //get the path between each series and the average
+            for (int i = 0; i < series.Count; i++)
+            {
+                var dtw = new Dtw(new[] { new SeriesVariable(average, series[i]) });
+                var path = dtw.GetPath();
+            }
+
+            return average;
+        }
+
+
+    }
+}
