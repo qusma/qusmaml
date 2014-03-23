78e1d61a048f3531529bebbe725dc217d2090045
qusma <qusmablog@gmail.com>
2014-03-23 04:59:28 +0200

    Added DBATest file.

---
+++ QUSMAMLTest/DBATest.cs
@@ -0,0 +1,24 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="DBATest.cs" company="">
+// Copyright 2014 Alexander Soffronow Pagonidis
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.Collections.Generic;
+using NUnit.Framework;
+using QUSMAML;
+
+namespace QUSMAMLTest
+{
+    [TestFixture]
+    public class DBATest
+    {
+        [Test]
+        public void Blah()
+        {
+            List<double[]> series = new List<double[]>();
+            
+            DBA.Average(series);
+        }
+    }
+}
